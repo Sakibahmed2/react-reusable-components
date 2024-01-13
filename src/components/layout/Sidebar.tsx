66c4a623dd6e715d279858f645ee6af7faf81290
unknown <mauijaloskor@gmail.com>
2024-01-13 11:41:10 +0600

    first commit

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen bg-red-500">
+      <h1>Side bar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
