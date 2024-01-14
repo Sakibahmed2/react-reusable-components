e888d87807239aff792e4f66388af34a52b4550d
unknown <mauijaloskor@gmail.com>
2024-01-14 13:14:32 +0600

    Basic Modal Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Button from "./components/ui/Button";
 import Container from "./components/ui/Container";
+import Modal from "./components/ui/Modal";
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const handleModalClose = () => {
+    setModal((prev) => !prev);
+  };
+
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
-        <Button />
+        <Button onClick={() => setModal((prev) => !prev)}>Open modal</Button>
+        <Modal isOpen={modal} onClose={handleModalClose}>
+          <h2>This is modal</h2>
+        </Modal>
       </div>
     </Container>
   );
