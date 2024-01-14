e55c9ead97a6c547810545daa9e7cfb2b8502cd2
unknown <mauijaloskor@gmail.com>
2024-01-14 11:54:54 +0600

    Solution Of Merge Problem With Cn Utility Function

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="h-screen w-full flex justify-center items-center">
         <button className="btn btn-primary">This is a button</button>
         <button className="btn btn-danger">This is a button</button>
-        <Button className="bg-red-500" />
+        <Button outline={true} />
       </div>
     </Container>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { twMerge } from "tailwind-merge";
+import { clsx } from "clsx";
+import cn from "../../utils/cn";
 
-const Button = ({ className }) => {
-  return <button className={`bg-green-500 ${className}`}>Click</button>;
+// `bg-purple-500 ${className} ${
+//   variant === "outline" ? "border border-purple-500 bg-opacity-10" : ""
+// } `
+
+const Button = ({ className, outline }) => {
+  return (
+    <button
+      className={cn(
+        "bg-purple-500 px-3 py-2 rounded-md",
+        {
+          "border border-purple-500 bg-opacity-10": outline,
+        },
+        className
+      )}
+    >
+      Click
+    </button>
+  );
 };
 
 export default Button;
